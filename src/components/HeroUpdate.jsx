de16ce9f61ba8bae0e7b0d5daa4517a2be013c4c
Vincent Palomo <vincentpalomo@yahoo.com>
2023-10-11 17:56:32 -0500

    displaying all content needed for agent page

--- src/components/HeroUpdate.jsx
+++ src/components/HeroUpdate.jsx
@@ -20,7 +20,35 @@ const HeroUpdate = () => {
   return (
     <div>
       <div>hello world</div>
-      <div>{agents.data}</div>
+      <div>
+        {agents.map((agent) => {
+          return (
+            <div key={agent.uuid}>
+              {agent.isPlayableCharacter && (
+                <div>
+                  <h1 className="text-9xl">{agent.displayName}</h1>
+                  <p>{agent.description}</p>
+                  <p>{agent.role.displayName}</p>
+                  <div>
+                    {agent.abilities.map((ability, i) => {
+                      return (
+                        <div key={i}>
+                          <div>
+                            <img src={ability.displayIcon} alt={ability.displayName} />
+                            <p>{ability.displayName}</p>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <img className="" src={agent.bustPortrait} />
+                  <img src={agent.background} alt={agent.displayName} />
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
