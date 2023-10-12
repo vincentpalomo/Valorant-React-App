a7840f151460e691a3d90a3a961c89f55e4818b9
Vincent Palomo <vincentpalomo@yahoo.com>
2023-10-12 17:57:25 -0500

    updated agent component

--- src/components/Agents.jsx
+++ src/components/Agents.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchAgents } from '../api/api';
 
-const Agents = () => {
+const HeroUpdate = () => {
   const [agents, setAgents] = useState([]);
 
   useEffect(() => {
@@ -14,47 +14,60 @@ const Agents = () => {
       console.log(agent);
       setAgents(agent.data);
     } catch (err) {
-      console.error('error fetching agents line 13 Hero jsx', err);
+      console.error('error fetching agents', err);
     }
   };
   return (
-    <div className="grid sm:grid-row-3 gap-5 justify-center items-center my-3">
-      {agents.map((agent) => {
-        return (
-          <div className="max-w-5xl" key={agent.uuid}>
-            {agent.isPlayableCharacter && (
-              <div className="card card-side bg-base-100 shadow-xl">
-                <figure>
-                  <img className="h-[40rem] sm:h-[50rem] sm:w-[125rem]" src={agent.bustPortrait} alt="Agents" />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title font-sans text-4xl">{agent.displayName}</h2>
-                  <p>Bio: {agent.description}</p>
-                  <p>Role: {agent.role.displayName}</p>
-                  <p>Abilities: </p>
-                  <div>
-                    {agent.abilities.map((ability, i) => {
-                      return (
-                        <div key={i}>
-                          <span className="flex flex-row justify-center items-center gap-3">
-                            <img src={ability.displayIcon} alt={ability.displayName} className="h-20 w-20" />
-                            <p>{ability.displayName}</p>
-                          </span>
-                        </div>
-                      );
-                    })}
+    <div className="mt-10 sm:m-0">
+      <div className="grid grid-cols-1 gap-2 sm:gap-0 sm:m-0">
+        {agents.map((agent) => {
+          return (
+            <div key={agent.uuid}>
+              {agent.isPlayableCharacter && (
+                <div className="flex flex-col items-center gap-2 sm:gap-0 sm:flex-row">
+                  <div className="flex flex-col items-center sm:justify-center sm:gap-5 gap-1 sm:h-[60rem] sm:bg-base-200 sm:w-4/5">
+                    <h1 className="text-8xl sm:text-[14rem] sm:tracking-[-.045em] text-slate-100 font-Mohave font-bold tracking-tight uppercase">
+                      {agent.displayName}
+                    </h1>
+                    <p className="text-2xl sm:text-[5rem] sm:tracking-tighter text-slate-50 font-Mohave font-bold uppercase">
+                      {agent.role.displayName}
+                    </p>
+                    <span className="hidden sm:block sm:border-b-2 sm:w-4/5"></span>
+                    <p className="w-4/5 sm:w-1/3 sm:text-xl sm:leading-5 leading-4">{agent.description}</p>
+                    <div className="flex space-x-1">
+                      {agent.abilities.map((ability, i) => {
+                        return (
+                          <div key={i}>
+                            <div className="flex flex-col justify-center items-center">
+                              <img
+                                src={ability.displayIcon}
+                                alt={ability.displayName}
+                                className="h-10 w-10 sm:h-32 sm:w-32"
+                              />
+                              <p className="sm:uppercase sm:font-Mohave sm:font-black sm:text-xl">
+                                {ability.displayName}
+                              </p>
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </div>
                   </div>
-                  {/* <div className='card-actions justify-end'>
-                    <button className='btn btn-primary'>Skills</button>
-                  </div> */}
+                  <div className="relative h-[24rem] sm:h-full overflow-hidden">
+                    <img
+                      className="absolute scale-[2.5] sm:scale-[2.5] left-32 top-52 sm:top-72 sm:left-48 z-10"
+                      src={agent.bustPortrait}
+                    />
+                    <img className="scale-105 z-0" src={agent.background} alt={agent.displayName} />
+                  </div>
                 </div>
-              </div>
-            )}
-          </div>
-        );
-      })}
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-export default Agents;
+export default HeroUpdate;
