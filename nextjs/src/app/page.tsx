e1bf80e8fe449f7ff72c3266d382de13ec9fab8b
Vincent Palomo <vincentpalomo@yahoo.com>
2024-01-31 16:57:49 -0600

    nextjs not working with fetching data

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,8 +1,35 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { fetchAgents } from './api/api';
+
 export default function Home() {
+  const [agents, setAgents] = useState([]);
+
+  useEffect(() => {
+    getAgents();
+  }, []);
+
+  const getAgents = async () => {
+    try {
+      const agent = await fetchAgents();
+      console.log(agent);
+      setAgents(agent?.data);
+    } catch (error) {
+      console.error('error fetching agents', error);
+    }
+  };
+
+  console.log([agents]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div></div>
-      <div>hello from nextjs</div>
+      {/* <div>
+        {agents.map((agent) => {
+          return <div key={agent.uuid}>{agent.isPlayableCharacter && <div>{agent.displayName}</div>}</div>;
+        })}
+      </div>
+      <div>hello from nextjs</div> */}
     </main>
   );
 }
