5e16125285bf196c93d4c2b1cce194de1c6900e9
Vincent Palomo <vincentpalomo@yahoo.com>
2024-02-06 16:27:10 -0600

    creating new folder for valorant site, will not be using nextjs

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import { fetchAgents } from './api/api';
-
 export default function Home() {
-  const [agents, setAgents] = useState([]);
+  const [agents, setAgents] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     getAgents();
@@ -12,24 +13,43 @@ export default function Home() {
 
   const getAgents = async () => {
     try {
-      const agent = await fetchAgents();
-      console.log(agent);
-      setAgents(agent?.data);
+      const agentResponse: any = await fetchAgents();
+      if (agentResponse.status === 200) {
+        setAgents(agentResponse.data);
+        setLoading(false);
+      } else {
+        setError(`HTTP Error: ${agentResponse.status}`);
+      }
     } catch (error) {
-      console.error('error fetching agents', error);
+      console.error('Error fetching agents:', error);
+      setError('Error fetching agents');
     }
   };
 
-  console.log([agents]);
+  console.log('Agents:', agents); // Log agents state
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  console.log('Rendering'); // Log when component renders
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <div>
-        {agents.map((agent) => {
-          return <div key={agent.uuid}>{agent.isPlayableCharacter && <div>{agent.displayName}</div>}</div>;
-        })}
+      <div>
+        {agents.length > 0 ? (
+          agents.map((agent: any) => {
+            console.log('Agent:', agent); // Log each agent
+            return <div key={agent.uuid}>{agent.displayName}</div>;
+          })
+        ) : (
+          <div>No agents found.</div>
+        )}
       </div>
-      <div>hello from nextjs</div> */}
     </main>
   );
 }
---
+++ valorant_update/src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
