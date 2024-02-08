a27c3e1147c3a5c7b2c1d29453a561a3c681edd6
Vincent Palomo <vincentpalomo@yahoo.com>
2024-02-07 18:30:08 -0600

    updated styling for about section

--- valorant_update/src/components/Main.jsx
+++ valorant_update/src/components/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Main = () => {
   return (
+    // mobile view
     <>
       <div className="flex flex-col justify-end items-center h-screen">
         <div className="mb-[150px] flex flex-col items-center">
@@ -11,19 +12,25 @@ const Main = () => {
           </p>
         </div>
       </div>
-      <div className="h-screen bg-neutral-200 flex flex-col justify-center gap-5">
-        <div className="text-[40px] leading-3 font-Playfair italic text-valorant-black">we are</div>
-        <div className="text-8xl font-Mohave font-bold tracking-tighter uppercase text-valorant-black">valorant</div>
+      <div className="h-screen bg-valorant-white flex flex-col justify-center gap-5">
+        <div>
+          <div className="text-[40px] font-Playfair italic text-valorant-black mt-10 mx-9">we are</div>
+          <div className="text-8xl font-Mohave font-bold tracking-tighter leading-[72px] uppercase text-valorant-black mx-9">
+            valorant
+          </div>
+        </div>
 
-        <div className="text-[23.04px]">defy the limits</div>
-        <p className="text-[19.20px]">
+        <div className="text-[23.04px] mx-9 uppercase font-bold">defy the limits</div>
+        <p className="text-[19.20px] text-valorant-grey tracking-wider leading-tight mx-9">
           Blend your style and experience on a global, competitive stage. You have 13 rounds to attack and defend your
           side using sharp gunplay and tactical abilities. And, with one life per-round, you'll need to think faster
           than your opponent if you want to survive. Take on foes across Competitive and Unranked modes as well as
           Deathmatch and Spike Rush
         </p>
-        <div className="flex justify-center">
-          <button className="h-[60px] w-[300px] bg-valorant-red rounded text-valorant-white">learn the game</button>
+        <div className="flex justify-center mt-10">
+          <button className="h-[60px] w-[300px] bg-valorant-red rounded text-valorant-white font-bold uppercase">
+            learn the game
+          </button>
         </div>
       </div>
     </>
