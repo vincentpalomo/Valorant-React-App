d6d7dc2c558e498f5d123619ea65066672b109b8
Vincent Palomo <vincentpalomo@yahoo.com>
2024-02-12 18:10:18 -0600

    updated agent component to fetch data from api

--- valorant_update/src/components/Agents.jsx
+++ valorant_update/src/components/Agents.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchAllAgents } from '../api/api';
 
 const Agents = () => {
+  const [agents, setAgents] = useState([]);
+
+  useEffect(() => {
+    fetchAgent();
+  }, []);
+
+  const fetchAgent = async () => {
+    try {
+      const agents = await fetchAllAgents();
+
+      if (!agents) throw new Error('Could not retrieve Agents...');
+
+      setAgents(agents.data);
+    } catch (error) {
+      console.error('error fetching agents', error);
+    }
+  };
+
+  console.log(agents.data);
+
   return (
-    <div className="h-screen bg-valorant-red flex justify-center items-center">
-      <div>Agents will be created here. come back soon</div>
+    <div className="h-screen bg-valorant-red flex flex-col justify-center items-center">
+      <div className="font-Playfair text-off-white">showing data from fetch object</div>
+      <div>
+        {agents.data?.map((agent) => {
+          return <div key={agent.uuid}>{agent.isPlayableCharacter && <div>{agent.displayName}</div>}</div>;
+        })}
+      </div>
     </div>
   );
 };
