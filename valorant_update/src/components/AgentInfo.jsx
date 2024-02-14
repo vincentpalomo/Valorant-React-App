6f5b56214b73690dcf07a3adc212f5800c43535b
Vincent Palomo <vincentpalomo@yahoo.com>
2024-02-13 22:15:38 -0600

    added agent images to agent info page

--- valorant_update/src/components/AgentInfo.jsx
+++ valorant_update/src/components/AgentInfo.jsx
@@ -27,7 +27,7 @@ const AgentInfo = ({ agentId, onClose }) => {
   return (
     <>
       {/* mobile view */}
-      <div className="h-screen w-screen bg-valorant-black flex flex-col justify-center items-start px-5 gap-5">
+      <div className="sm:hidden h-screen w-screen bg-valorant-black flex flex-col justify-center items-start px-5 gap-5">
         <div className="flex flex-col items-start gap-1 mt-20">
           <button onClick={handleGoBack} className="text-valorant-red uppercase font-sans font-bold z-10">
             back
@@ -61,6 +61,47 @@ const AgentInfo = ({ agentId, onClose }) => {
             );
           })}
         </div>
+      </div>
+
+      {/* desktop view */}
+      <div className="hidden h-screen w-screen bg-valorant-black sm:flex flex-col justify-center items-start px-20 gap-5 relative overflow-hidden">
+        <div className="flex flex-col items-start gap-1">
+          <button onClick={handleGoBack} className="text-valorant-red uppercase font-sans font-bold z-10">
+            back
+          </button>
+          <div className="font-Mohave font-bold uppercase tracking-tighter leading-[6rem] text-valorant-white text-[160px] -ml-2 mt-20">
+            {agent?.data.displayName}
+          </div>
+        </div>
+
+        <div className="font-Playfair italic text-valorant-red text-[64px] leading-10">
+          {agent?.data.role.displayName}
+        </div>
+
+        <p className="font-sans text-[19.2px] text-valorant-white leading-tight tracking-wider w-[390px] mb-16">
+          {agent?.data.description}
+        </p>
+
+        <div className="flex justify-center items-center gap-3">
+          {agent?.data.abilities.map((ability) => {
+            return (
+              <div className="flex flex-col items-center" key={ability.slot}>
+                <img className="h-28" src={ability.displayIcon} alt={ability.displayName} />
+              </div>
+            );
+          })}
+        </div>
+
+        <img
+          className="absolute scale-[.60] left-96 -bottom-64 2xl:scale-50 2xl:-bottom-[425px] z-10"
+          src={agent?.data.fullPortrait}
+          alt={agent?.data.displayName}
+        />
+        <img
+          className="absolute scale-[.80] right-0 top-0 2xl:scale-100 2xl:top-10 2xl:right-56"
+          src={agent?.data.background}
+          alt={agent?.data.displayName}
+        />
       </div>
     </>
   );
