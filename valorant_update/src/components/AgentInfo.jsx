1e268a27c2ff5d4c5c1c0d1df17a9490fff539e8
Vincent Palomo <vincentpalomo@yahoo.com>
2024-02-13 20:06:11 -0600

    agent info page for mobile completed

--- valorant_update/src/components/AgentInfo.jsx
+++ valorant_update/src/components/AgentInfo.jsx
@@ -27,27 +27,36 @@ const AgentInfo = ({ agentId, onClose }) => {
   console.log('agent:', agent?.data);
 
   return (
-    <div className="h-screen w-screen bg-valorant-black flex flex-col justify-center items-center gap-5">
-      <button onClick={handleGoBack} className="text-valorant-red">
-        go back
-      </button>
-      <div className="text-valorant-white">{agent?.data.displayName}</div>
-
-      <div className="h-[400px] relative w-full overflow-hidden">
-        <img
-          className="absolute -bottom-10 scale-[2] z-10"
-          src={agent?.data.fullPortrait}
-          alt={agent?.data.displayName}
-        />
-        <img className="absolute -bottom-28 scale-[1.5]" src={agent?.data.background} alt={agent?.data.displayName} />
+    <div className="h-screen w-screen bg-valorant-black flex flex-col justify-center items-start px-5 gap-5">
+      <div className="flex flex-col items-start gap-1 mt-20">
+        <button onClick={handleGoBack} className="text-valorant-red uppercase font-sans font-bold z-10">
+          back
+        </button>
+        <div className="font-Mohave font-bold uppercase tracking-tighter leading-[5.5rem] text-valorant-white text-8xl -ml-2">
+          {agent?.data.displayName}
+        </div>
       </div>
 
-      <div className="flex">
+      {/* agent icon */}
+      <div className="h-[250px] -my-2 w-full flex items-center justify-center">
+        <img src={agent?.data.displayIcon} alt={agent?.data.displayName} />
+        {/* <img className="absolute -bottom-28 scale-[1.5]" src={agent?.data.background} alt={agent?.data.displayName} /> */}
+      </div>
+
+      <div className="font-Playfair italic text-valorant-red text-4xl">{agent?.data.role.displayName}</div>
+
+      <p className="font-sans text-[19.2px] text-valorant-white leading-tight tracking-wide">
+        {agent?.data.description}
+      </p>
+
+      {/* <div>abilites:</div> */}
+
+      <div className="flex justify-center items-center gap-3">
         {agent?.data.abilities.map((ability) => {
           return (
             <div className="flex flex-col items-center">
-              <img src={ability.displayIcon} alt={ability.displayName} />
-              <div>{ability.displayName}</div>
+              <img className="h-14" src={ability.displayIcon} alt={ability.displayName} />
+              {/* <div>{ability.displayName}</div> */}
             </div>
           );
         })}
