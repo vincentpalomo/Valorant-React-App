0f7843f89b19a4a5cb5fb60e30d115d4a4ff1d6e
Vincent Palomo <vincentpalomo@yahoo.com>
2024-02-14 20:52:47 -0600

    map component completed

--- valorant_update/src/components/Maps.jsx
+++ valorant_update/src/components/Maps.jsx
@@ -25,7 +25,7 @@ const Maps = () => {
   return (
     <>
       {/* mobile view */}
-      <div className="h-full flex flex-col justify-center items-center bg-valorant-white">
+      <div className="sm:hidden h-full flex flex-col justify-center items-center bg-valorant-white">
         {/* <div className="font-tungsten text-valorant-black uppercase text-8xl mt-24">maps</div> */}
         <div className="mt-24">
           {maps.data?.map((map) => {
@@ -41,6 +41,29 @@ const Maps = () => {
                   alt={map.displayName}
                 />
                 <img className="opacity-75" src={map.splash} alt={map.displayName} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* desktop view */}
+      <div className="h-full hidden sm:flex flex-col justify-center items-center bg-valorant-black">
+        {/* <div className="font-tungsten text-valorant-black uppercase text-8xl mt-24">maps</div> */}
+        <div className="mt-24">
+          {maps.data?.map((map) => {
+            return (
+              <div key={map.uuid} className="relative text-valorant-white">
+                <div className="font-tungsten uppercase text-9xl right-10 absolute z-10">{map.displayName}</div>
+                <div className="font-sans leading-snug tracking-wider w-[50%] absolute bottom-32 2xl:bottom-48 left-10 text-left z-10 text-valorant-white">
+                  {map.narrativeDescription}
+                </div>
+                <img
+                  className="absolute scale-[.55] 2xl:scale-[.6] z-10 bottom-0 2xl:bottom-20"
+                  src={map.displayIcon}
+                  alt={map.displayName}
+                />
+                <img className="2xl:h-screen 2xl:w-screen opacity-60" src={map.splash} alt={map.displayName} />
               </div>
             );
           })}
