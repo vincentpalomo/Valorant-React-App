e3737e8fe1753f2f59375218559294cca65057ea
Vincent Palomo <vincentpalomo@yahoo.com>
2024-03-04 17:57:39 -0600

    updated styling of multiple elements in agents and agentsinfo to better fit macbook screen

--- valorant_update/src/components/AgentInfo.jsx
+++ valorant_update/src/components/AgentInfo.jsx
@@ -27,33 +27,31 @@ const AgentInfo = ({ agentId, onClose }) => {
   return (
     <>
       {/* mobile view */}
-      <div className="sm:hidden h-screen w-screen bg-valorant-black flex flex-col justify-center items-start px-5 gap-5">
-        <div className="flex flex-col items-start gap-2 mt-20">
-          <button onClick={handleGoBack} className="text-valorant-red uppercase font-sans font-bold z-10">
+      <div className='sm:hidden h-screen w-screen bg-valorant-black flex flex-col justify-center items-start px-5 gap-5'>
+        <div className='flex flex-col items-start gap-2 mt-20'>
+          <button onClick={handleGoBack} className='text-valorant-red uppercase font-sans font-bold z-10'>
             back
           </button>
-          <div className="font-tungsten font-bold uppercase tracking-normal leading-[5.5rem] text-valorant-white text-8xl -ml-2">
+          <div className='font-tungsten font-bold uppercase tracking-normal leading-[5.5rem] text-valorant-white text-8xl -ml-2'>
             {agent?.data.displayName}
           </div>
         </div>
 
         {/* agent icon */}
-        <div className="h-[250px] -my-2 w-full flex items-center justify-center bg-valorant-red rounded-md overflow-clip">
+        <div className='h-[250px] -my-2 w-full flex items-center justify-center bg-valorant-red rounded-md overflow-clip'>
           <img src={agent?.data.displayIcon} alt={agent?.data.displayName} />
           {/* <img className="absolute -bottom-28 scale-[1.5]" src={agent?.data.background} alt={agent?.data.displayName} /> */}
         </div>
 
-        <div className="font-Playfair italic text-valorant-red text-5xl">{agent?.data.role.displayName}</div>
+        <div className='font-Playfair italic text-valorant-red text-5xl'>{agent?.data.role.displayName}</div>
 
-        <p className="font-sans text-[19.2px] text-valorant-white leading-tight tracking-wide">
-          {agent?.data.description}
-        </p>
+        <p className='font-sans text-[19.2px] text-valorant-white leading-tight tracking-wide'>{agent?.data.description}</p>
 
-        <div className="flex justify-center items-center gap-3 w-full pt-5">
+        <div className='flex justify-center items-center gap-3 w-full pt-5'>
           {agent?.data.abilities.map((ability) => {
             return (
-              <div className="flex flex-col items-center" key={ability.slot}>
-                <img className="h-14" src={ability.displayIcon} alt={ability.displayName} />
+              <div className='flex flex-col items-center' key={ability.slot}>
+                <img className='h-14' src={ability.displayIcon} alt={ability.displayName} />
                 {/* <div>{ability.displayName}</div> */}
               </div>
             );
@@ -62,44 +60,37 @@ const AgentInfo = ({ agentId, onClose }) => {
       </div>
 
       {/* desktop view */}
-      <div className="hidden h-screen w-screen bg-valorant-black sm:flex flex-col justify-center items-start px-20 gap-5 relative overflow-hidden">
-        <div className="flex flex-col items-start gap-1">
-          <button
-            onClick={handleGoBack}
-            className="text-valorant-red uppercase font-sans font-bold z-10 hover:text-valorant-white"
-          >
+      <div className='hidden h-screen w-screen bg-valorant-black sm:flex flex-col justify-center items-start px-20 gap-5 relative overflow-hidden'>
+        <div className='flex flex-col items-start gap-1'>
+          <button onClick={handleGoBack} className='text-valorant-red uppercase font-sans font-bold z-10 hover:text-valorant-white mt-10 2xl:mt-0'>
             back
           </button>
-          <div className="font-tungsten font-bold uppercase tracking-normal leading-[6rem] text-valorant-white text-[160px] -ml-2 mt-20">
+          <div className='font-tungsten font-bold uppercase tracking-normal leading-[6rem] text-valorant-white text-[160px] -ml-2 2xl:mt-20 mt-10'>
             {agent?.data.displayName}
           </div>
         </div>
 
-        <div className="font-Playfair italic text-valorant-red text-[64px] leading-[2.7rem]">
-          {agent?.data.role.displayName}
-        </div>
+        <div className='font-Playfair italic text-valorant-red text-[64px] leading-[2.7rem]'>{agent?.data.role.displayName}</div>
 
-        <p className="font-sans text-[19.2px] text-valorant-white leading-tight tracking-wider w-[390px] mb-16">
-          {agent?.data.description}
-        </p>
+        <p className='font-sans text-[19.2px] text-valorant-white leading-tight tracking-wider w-[390px] mb-16'>{agent?.data.description}</p>
 
-        <div className="flex justify-center items-center gap-3">
+        <div className='flex justify-center items-center gap-3'>
           {agent?.data.abilities.map((ability) => {
             return (
-              <div className="flex flex-col items-center" key={ability.slot}>
-                <img className="h-28" src={ability.displayIcon} alt={ability.displayName} />
+              <div className='flex flex-col items-center' key={ability.slot}>
+                <img className='h-28' src={ability.displayIcon} alt={ability.displayName} />
               </div>
             );
           })}
         </div>
 
         <img
-          className="absolute scale-[.60] left-96 -bottom-64 2xl:scale-50 2xl:-bottom-[425px] z-10"
+          className='absolute scale-[.50] left-96 -bottom-72 2xl:scale-50 2xl:-bottom-[425px] z-10'
           src={agent?.data.fullPortrait}
           alt={agent?.data.displayName}
         />
         <img
-          className="absolute scale-[.80] right-0 top-0 2xl:scale-100 2xl:top-10 2xl:right-56"
+          className='absolute scale-[.80] right-0 -top-10 2xl:scale-100 2xl:top-10 2xl:right-56'
           src={agent?.data.background}
           alt={agent?.data.displayName}
         />
--- valorant_update/src/components/Agents.jsx
+++ valorant_update/src/components/Agents.jsx
@@ -41,58 +41,52 @@ const Agents = () => {
   return (
     <>
       {/* mobile */}
-      <div className="h-screen bg-valorant-white sm:hidden flex flex-col justify-center gap-4 relative">
-        <div className="font-Mohave font-bold uppercase tracking-tighter text-valorant-red text-8xl ml-5 mt-20">
-          agents
-        </div>
+      <div className='h-screen bg-valorant-white sm:hidden flex flex-col justify-center gap-4 relative'>
+        <div className='font-Mohave font-bold uppercase tracking-tighter text-valorant-red text-8xl ml-5 mt-20'>agents</div>
 
         {/* agent images */}
-        <div className="h-[300px] relative mb-1 overflow-x-clip">
+        <div className='h-[300px] relative mb-1 overflow-x-clip'>
           <img
-            className="absolute right-24 -bottom-6 scale-[.9] z-0"
-            src="https://media.valorant-api.com/agents/8e253930-4c05-31dd-1b6c-968525494517/fullportrait.png"
-            alt="omen"
+            className='absolute right-24 -bottom-6 scale-[.9] z-0'
+            src='https://media.valorant-api.com/agents/8e253930-4c05-31dd-1b6c-968525494517/fullportrait.png'
+            alt='omen'
           />
           <img
-            className="absolute -bottom-2 scale-[1.1] z-10"
-            src="https://media.valorant-api.com/agents/f94c3b30-42be-e959-889c-5aa313dba261/fullportrait.png"
-            alt="raze"
+            className='absolute -bottom-2 scale-[1.1] z-10'
+            src='https://media.valorant-api.com/agents/f94c3b30-42be-e959-889c-5aa313dba261/fullportrait.png'
+            alt='raze'
           />
           <img
-            className="absolute left-24 -bottom-6 scale-[.9]"
-            src="https://media.valorant-api.com/agents/1e58de9c-4950-5125-93e9-a0aee9f98746/fullportrait.png"
-            alt="killjoy"
+            className='absolute left-24 -bottom-6 scale-[.9]'
+            src='https://media.valorant-api.com/agents/1e58de9c-4950-5125-93e9-a0aee9f98746/fullportrait.png'
+            alt='killjoy'
           />
         </div>
 
-        <div className="font-Mohave font-bold uppercase tracking-tighter text-valorant-black text-5xl relative ml-5 whitespace-pre-wrap">
-          check{' '}
-          <span className="font-Playfair italic font-normal lowercase text-valorant-red text-[40px] absolute bottom-3 -mx-1">
-            the
-          </span>
+        <div className='font-Mohave font-bold uppercase tracking-tighter text-valorant-black text-5xl relative ml-5 whitespace-pre-wrap'>
+          check <span className='font-Playfair italic font-normal lowercase text-valorant-red text-[40px] absolute bottom-3 -mx-1'>the</span>
           {'     '}
           headcount
         </div>
 
-        <p className="font-sans text-valorant-grey leading-tight tracking-wide text-[19.20px] w-[325px] ml-5">
-          Find more ways to plant the Spike on your enemies with scrappers, strategists, and hunters of every
-          description.
+        <p className='font-sans text-valorant-grey leading-tight tracking-wide text-[19.20px] w-[325px] ml-5'>
+          Find more ways to plant the Spike on your enemies with scrappers, strategists, and hunters of every description.
         </p>
 
         {/* select agent */}
-        <div className="relative ml-5 mt-5">
+        <div className='relative ml-5 mt-5'>
           <button
             onClick={handleOpen}
-            className="bg-valorant-white w-[350px] h-[60px] border border-valorant-grey font-sans font-bold uppercase tracking-wide text-left pl-5"
+            className='bg-valorant-white w-[350px] h-[60px] border border-valorant-grey font-sans font-bold uppercase tracking-wide text-left pl-5'
           >
             {selectedAgent ? selectedAgent : 'Select an agent'}
           </button>
           {open && (
-            <ul className="absolute border border-valorant-grey max-h-[300px] overflow-y-auto bg-valorant-white bottom-[60px] z-20">
+            <ul className='absolute border border-valorant-grey max-h-[300px] overflow-y-auto bg-valorant-white bottom-[60px] z-20'>
               {playableAgents?.map((agent) => {
                 return (
                   <div
-                    className="hover:bg-valorant-red h-[60px] w-[348px] font-tungsten font-bold uppercase tracking-normal text-5xl flex items-center pl-5"
+                    className='hover:bg-valorant-red h-[60px] w-[348px] font-tungsten font-bold uppercase tracking-normal text-5xl flex items-center pl-5'
                     key={agent.uuid}
                     onClick={() => handleAgentSelect(agent.uuid)}
                   >
@@ -103,41 +97,37 @@ const Agents = () => {
             </ul>
           )}
         </div>
-        <div className="absolute z-40">{selectedAgent && <AgentInfo agentId={selectedAgent} onClose={noAgent} />}</div>
+        <div className='absolute z-40'>{selectedAgent && <AgentInfo agentId={selectedAgent} onClose={noAgent} />}</div>
       </div>
 
       {/* desktop */}
-      <div className="hidden h-screen bg-valorant-white sm:flex flex-col justify-center items-center relative">
-        <div className="absolute top-[250px] font-Mohave font-bold uppercase tracking-tighter text-valorant-red text-[300px] leading-3 z-10">
+      <div className='hidden h-screen bg-valorant-white sm:flex flex-col justify-center items-center relative'>
+        <div className='absolute 2xl:top-[250px] top-[240px] font-Mohave font-bold uppercase tracking-tighter text-valorant-red text-[300px] leading-3 z-10'>
           agents
         </div>
 
-        <div className="font-Mohave font-bold uppercase tracking-tighter text-valorant-black text-8xl relative ml-5 whitespace-pre-wrap mb-[105px] z-10">
-          check{' '}
-          <span className="font-Playfair italic font-normal lowercase text-valorant-red text-[64px] absolute bottom-8 -mx-1">
-            the
-          </span>
+        <div className='font-Mohave font-bold uppercase tracking-tighter text-valorant-black text-8xl relative ml-5 whitespace-pre-wrap 2xl:mb-[105px] mb-[16px] z-10'>
+          check <span className='font-Playfair italic font-normal lowercase text-valorant-red text-[64px] absolute bottom-8 -mx-1'>the</span>
           {'    '}
           headcount
         </div>
 
         {/* agent select */}
-        <div className="absolute h-[450px] w-[1350px] bg-valorant-black flex justify-around items-center bottom-5 z-10">
-          <p className="font-sans text-valorant-grey leading-tight tracking-wide text-[19.20px] w-[600px] text-center absolute top-3">
-            Find more ways to plant the Spike on your enemies with scrappers, strategists, and hunters of every
-            description.
+        <div className='absolute 2xl:h-[450px] h-[350px] w-[1350px] bg-valorant-black flex justify-around items-center bottom-5 z-10'>
+          <p className='font-sans text-valorant-grey leading-tight tracking-wide text-[19.20px] w-[600px] text-center absolute 2xl:top-3 top-1'>
+            Find more ways to plant the Spike on your enemies with scrappers, strategists, and hunters of every description.
           </p>
 
-          <div className="text-off-white flex justify-around h-[70%] w-full">
+          <div className='text-off-white flex justify-around 2xl:h-[70%] h-[80%] w-full'>
             {/* Duelist Agents */}
-            <div className="w-[275px] flex flex-col items-center">
-              <h2 className="text-valorant-white font-Playfair italic lowercase text-[64px]">Duelists</h2>
+            <div className='w-[275px] flex flex-col items-center'>
+              <h2 className='text-valorant-white font-Playfair italic lowercase text-[64px]'>Duelists</h2>
               {playableAgents
                 ?.filter((agent) => agent.role.displayName === 'Duelist')
                 .map((agent) => (
                   <div
                     onClick={() => handleAgentSelect(agent.uuid)}
-                    className="text-valorant-red cursor-pointer text-[19.2px] hover:border-b-2 hover:border-valorant-white"
+                    className='text-valorant-red cursor-pointer text-[19.2px] hover:border-b-2 hover:border-valorant-white'
                     key={agent.uuid}
                   >
                     {agent.displayName}
@@ -146,14 +136,14 @@ const Agents = () => {
             </div>
 
             {/* Controller Agents */}
-            <div className="w-[275px] flex flex-col items-center">
-              <h2 className="text-valorant-white font-Playfair italic lowercase text-[64px]">Controllers</h2>
+            <div className='w-[275px] flex flex-col items-center'>
+              <h2 className='text-valorant-white font-Playfair italic lowercase text-[64px]'>Controllers</h2>
               {playableAgents
                 ?.filter((agent) => agent.role.displayName === 'Controller')
                 .map((agent) => (
                   <div
                     onClick={() => handleAgentSelect(agent.uuid)}
-                    className="text-valorant-red cursor-pointer text-[19.2px] hover:border-b-2 hover:border-valorant-white"
+                    className='text-valorant-red cursor-pointer text-[19.2px] hover:border-b-2 hover:border-valorant-white'
                     key={agent.uuid}
                   >
                     {agent.displayName}
@@ -162,14 +152,14 @@ const Agents = () => {
             </div>
 
             {/* Initiator Agents */}
-            <div className="w-[275px]  flex flex-col items-center">
-              <h2 className="text-valorant-white font-Playfair italic lowercase text-[64px]">Initiators</h2>
+            <div className='w-[275px]  flex flex-col items-center'>
+              <h2 className='text-valorant-white font-Playfair italic lowercase text-[64px]'>Initiators</h2>
               {playableAgents
                 ?.filter((agent) => agent.role.displayName === 'Initiator')
                 .map((agent) => (
                   <div
                     onClick={() => handleAgentSelect(agent.uuid)}
-                    className="text-valorant-red cursor-pointer text-[19.2px] hover:border-b-2 hover:border-valorant-white"
+                    className='text-valorant-red cursor-pointer text-[19.2px] hover:border-b-2 hover:border-valorant-white'
                     key={agent.uuid}
                   >
                     {agent.displayName}
@@ -178,14 +168,14 @@ const Agents = () => {
             </div>
 
             {/* Sentinel Agents */}
-            <div className="w-[275px] flex flex-col items-center">
-              <h2 className="text-valorant-white font-Playfair italic lowercase text-[64px]">Sentinels</h2>
+            <div className='w-[275px] flex flex-col items-center'>
+              <h2 className='text-valorant-white font-Playfair italic lowercase text-[64px]'>Sentinels</h2>
               {playableAgents
                 ?.filter((agent) => agent.role.displayName === 'Sentinel')
                 .map((agent) => (
                   <div
                     onClick={() => handleAgentSelect(agent.uuid)}
-                    className="text-valorant-red cursor-pointer text-[19.2px] hover:border-b-2 hover:border-valorant-white"
+                    className='text-valorant-red cursor-pointer text-[19.2px] hover:border-b-2 hover:border-valorant-white'
                     key={agent.uuid}
                   >
                     {agent.displayName}
@@ -194,18 +184,18 @@ const Agents = () => {
             </div>
           </div>
         </div>
-        <div className="absolute z-40">{selectedAgent && <AgentInfo agentId={selectedAgent} onClose={noAgent} />}</div>
+        <div className='absolute z-40'>{selectedAgent && <AgentInfo agentId={selectedAgent} onClose={noAgent} />}</div>
 
-        <div className="absolute w-full h-screen overflow-hidden">
+        <div className='absolute w-full h-screen overflow-hidden'>
           <img
-            className="absolute scale-[.6] 2xl:scale-50 z-0 right-[500px] -bottom-48 2xl:-bottom-96"
-            src="https://media.valorant-api.com/agents/f94c3b30-42be-e959-889c-5aa313dba261/fullportrait.png"
-            alt="raze"
+            className='absolute scale-[.6] 2xl:scale-50 z-0 right-[500px] -bottom-72 2xl:-bottom-96'
+            src='https://media.valorant-api.com/agents/f94c3b30-42be-e959-889c-5aa313dba261/fullportrait.png'
+            alt='raze'
           />
           <img
-            className="absolute scale-[.6] 2xl:scale-50 z-0 left-[500px] -bottom-48 2xl:-bottom-96"
-            src="https://media.valorant-api.com/agents/1e58de9c-4950-5125-93e9-a0aee9f98746/fullportrait.png"
-            alt="killjoy"
+            className='absolute scale-[.6] 2xl:scale-50 z-0 left-[500px] -bottom-72 2xl:-bottom-96'
+            src='https://media.valorant-api.com/agents/1e58de9c-4950-5125-93e9-a0aee9f98746/fullportrait.png'
+            alt='killjoy'
           />
         </div>
       </div>
